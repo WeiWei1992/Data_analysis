9988b70db356d88ee78c3dd04ed9f29402df5729
weiwei <wei_wei1992@yeah.net>
2020-07-11 12:45:38 +0800

    test

--- pandas_test/pandan_test.py
+++ pandas_test/pandan_test.py
@@ -35,26 +35,82 @@
 
 
 
-#练习2
+# #练习2
+# import pandas as pd
+# path2='./pandas_exercise/exercise_data/Euro2012_stats.csv'
+# euro12=pd.read_csv(path2)
+# print(euro12.head())
+#
+# #只选取某一列
+# print(euro12.Goals)
+# print(euro12['Goals'])
+#
+# #获取行数
+# print(euro12.shape[0])
+#
+# #试试一下git
+# #试一下git add
+#
+# #获取列
+# print(euro12.shape[1])
+#
+# discipline=euro12[['Team','Yellow Cards','Red Cards']]
+# print(discipline)
+# x=discipline.sort_values(['Red Cards','Yellow Cards'],ascending=False)
+# print(discipline)
+# print(round(discipline['Yellow Cards'].mean()))
+# print(euro12[euro12.Goals>6])
+# print(euro12[euro12.Team.str.startswith('G')])
+#
+# print(euro12.iloc[:,0:7])
+# print(euro12.iloc[:,:-3])
+#
+# print(euro12.loc[euro12.Team.isin(['England','Italy','Russia']),['Team','Shooting Accuracy']])
+
+
+# #练习3 数据分组
+# import pandas as pd
+#
+# path3='./pandas_exercise/exercise_data/drinks.csv'
+# drinks=pd.read_csv(path3)
+# print(drinks.head())
+# print(drinks.groupby('continent').beer_servings.mean())
+# print(drinks.groupby('continent').wine_servings.describe())
+# print(drinks.groupby('continent').median())
+# print(drinks.groupby('continent').beer_servings.agg(['mean','min','max']))
+
+
+##练习4 Apply函数
+import numpy as np
 import pandas as pd
-path2='./pandas_exercise/exercise_data/Euro2012_stats.csv'
-euro12=pd.read_csv(path2)
-print(euro12.head())
 
-#只选取某一列
-print(euro12.Goals)
-print(euro12['Goals'])
+path4='./pandas_exercise/exercise_data/US_Crime_Rates_1960_2014.csv'
+crime=pd.read_csv(path4)
+print(crime.head())
 
-#获取行数
-print(euro12.shape[0])
 
-#试试一下git
-#试一下git add
 
-#获取列
-print(euro12.shape[1])
-print("xxx")
 
-print("this is my pc")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
