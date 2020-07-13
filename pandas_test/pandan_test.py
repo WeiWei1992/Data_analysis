f293cfd82db46dac72bf6e86d908affe9dd24601
weiwei <wei_wei1992@yeah.net>
2020-07-13 15:48:44 +0800

    pandas

--- pandas_test/pandan_test.py
+++ pandas_test/pandan_test.py
@@ -81,36 +81,232 @@
 
 
 ##练习4 Apply函数
-import numpy as np
+# import numpy as np
+# import pandas as pd
+#
+# path4='./pandas_exercise/exercise_data/US_Crime_Rates_1960_2014.csv'
+# crime=pd.read_csv(path4)
+# print(crime.head())
+#
+# print(crime.info())
+# crime.Year=pd.to_datetime(crime.Year,format='%Y')
+# print(crime.info())
+#
+# crime=crime.set_index('Year',drop=True)
+# print(crime.head())
+#
+# del crime['Total']
+# print(crime.head())
+#
+# print(crime.idxmax(0))
+
+# #练习5 合并
+# import numpy as np
+# import pandas as pd
+# # path5='./pandas_exercise/exercise_data/US_Crime_Rates_1960_2014.csv'
+#
+# raw_data_1 = {
+#         'subject_id': ['1', '2', '3', '4', '5'],
+#         'first_name': ['Alex', 'Amy', 'Allen', 'Alice', 'Ayoung'],
+#         'last_name': ['Anderson', 'Ackerman', 'Ali', 'Aoni', 'Atiches']}
+#
+# raw_data_2 = {
+#         'subject_id': ['4', '5', '6', '7', '8'],
+#         'first_name': ['Billy', 'Brian', 'Bran', 'Bryce', 'Betty'],
+#         'last_name': ['Bonder', 'Black', 'Balwner', 'Brice', 'Btisan']}
+#
+# raw_data_3 = {
+#         'subject_id': ['1', '2', '3', '4', '5', '7', '8', '9', '10', '11'],
+#         'test_id': [51, 15, 15, 61, 16, 14, 15, 1, 61, 16]}
+# data1=pd.DataFrame(raw_data_1,columns=['subject_id','first_name','last_name'])
+# data2=pd.DataFrame(raw_data_2,columns=['subject_id','first_name','last_name'])
+# data3=pd.DataFrame(raw_data_3,columns=['subject_id','test_id'])
+# print(data1)
+# print(data2)
+#
+# all_data=pd.concat([data1,data2])
+# print(all_data)
+# print("xxxxxxx")
+# all_data_col=pd.concat([data1,data2],axis=1)
+# print(all_data_col)
+# print(data3)
+# print(pd.merge(all_data,data3,on='subject_id'))
+# print("=======================")
+# print(pd.merge(data1,data2,on='subject_id',how='inner'))
+# print(pd.merge(data1,data2,on='subject_id',how='outer'))
+
+
+# #练习6统计
+# import pandas as pd
+# import datetime
+# path6='./pandas_exercise/exercise_data/wind.data'
+# data1=pd.read_table(path6,sep='\s+')
+# print(data1)
+# data=pd.read_table(path6,sep='\s+',parse_dates=[[0,1,2]])
+# print(data.head())
+#
+# def fix_century(x):
+#     year=x.year-100 if x.year>1989 else x.year
+#     return datetime.date(year,x.month,x.day)
+# data['Yr_Mo_Dy']=data['Yr_Mo_Dy'].apply(fix_century)
+# print(data.head())
+#
+# data["Yr_Mo_Dy"]=pd.to_datetime(data["Yr_Mo_Dy"])
+# data=data.set_index('Yr_Mo_Dy')
+# print(data.head())
+# print(data.isnull().sum())
+# print(data.shape[0]-data.isnull().sum())
+#
+#
+# print(data.mean().mean())
+#
+# print("-------------")
+# print(data.mean(axis=0))
+#
+# loc_stats=pd.DataFrame()
+# loc_stats['min']=data.min()
+# loc_stats['max']=data.max()
+# loc_stats['mean']=data.mean()
+# loc_stats['std']=data.std()
+# print(loc_stats)
+# print(data.head)
+# day_stats = pd.DataFrame()
+#
+# # this time we determine axis equals to one so it gets each row.
+# day_stats['min'] = data.min(axis = 1) # min
+# day_stats['max'] = data.max(axis = 1) # max
+# day_stats['mean'] = data.mean(axis = 1) # mean
+# day_stats['std'] = data.std(axis = 1) # standard deviations
+# print(day_stats)
+#
+# data['date'] = data.index
+#
+# # creates a column for each value from date
+# data['month'] = data['date'].apply(lambda date: date.month)
+# data['year'] = data['date'].apply(lambda date: date.year)
+# data['day'] = data['date'].apply(lambda date: date.day)
+# print(data)
+#
+# january_winds=data.query('month==1')
+# print(january_winds)
+# print(january_winds.loc[:,'RPT':'ROS'].mean())
+#
+# print(data.query('day==1'))
+
+# #练习7 可视化
+# path7='./pandas_exercise/exercise_data/train.csv'
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import numpy as np
+#
+# titanic=pd.read_csv(path7)
+# print(titanic.head())
+# titanic.set_index('PassengerId').head()
+# x=titanic.set_index('PassengerId')
+# print(x.head())
+#
+# # males=(titanic['Sex']=='male').sum()
+# # females=(titanic['Sex']=='female').sum()
+# #
+# # proportions=[males,females]
+# # plt.pie(
+# #
+# #     proportions,
+# #     labels=['Males','Females'],
+# #     shadow=False,
+# #     colors=['blue','red'],
+# #     explode=(0.15,0),
+# #     startangle=90,
+# #     autopct='%1.1f%%'
+# # )
+# #
+# # plt.axis('equal')
+# # plt.title('Sex Proportion')
+# # plt.tight_layout()
+# # plt.show()
+#
+# # lm=sns.lmplot(x='Age',y='Fare',data=titanic,hue='Sex',fit_reg=False)
+# # lm.set(title='Fare x Age')
+# #
+# # axes=lm.axes
+# # axes[0,0].set_ylim(-5,)
+# # axes[0,0].set_xlim(-5,85)
+# #
+# # plt.show()
+#
+# print(titanic.Survived.sum())
+#
+# df=titanic.Fare.sort_values(ascending=False)
+# print(df)
+#
+# binsVal=np.arange(0,600,10)
+# print(binsVal)
+# plt.hist(df,bins=binsVal)
+# plt.xlabel('Fare')
+# plt.ylabel('Frequency')
+# plt.title('Fare Payed Histrogram')
+
+# plt.show()
+
+# #练习8 创建数据框
+# import pandas as pd
+# raw_data = {"name": ['Bulbasaur', 'Charmander','Squirtle','Caterpie'],
+#             "evolution": ['Ivysaur','Charmeleon','Wartortle','Metapod'],
+#             "type": ['grass', 'fire', 'water', 'bug'],
+#             "hp": [45, 39, 44, 45],
+#             "pokedex": ['yes', 'no','yes','no']
+#             }
+# print(raw_data)
+# pokemon=pd.DataFrame(raw_data)
+# print(pokemon.head())
+# pokemon=pokemon[['name','type','hp','evolution','pokedex']]
+# print(pokemon)
+#
+# pokemon['place'] = ['park','street','lake','forest']
+# print(pokemon)
+
+#print(pokemon.dtypes)
+
+
+# #练习9 时间序列
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# path9='./pandas_exercise/exercise_data/Apple_stock.csv'
+# apple=pd.read_csv(path9)
+# print(apple.head())
+# print(apple.dtypes)
+#
+# apple.Date=pd.to_datetime(apple.Date)
+# print(apple.Date.head())
+# apple=apple.set_index('Date')
+# print(apple.head())
+# print(apple.index.is_unique)
+# print(apple.sort_index(ascending=True).head())
+# print((apple.index.max()-apple.index.min()).days)
+
+#练习10 删除数据
+path10='./pandas_exercise/exercise_data/iris.csv'
 import pandas as pd
+import numpy as np
+iris=pd.read_csv(path10)
+print(iris.head())
 
-path4='./pandas_exercise/exercise_data/US_Crime_Rates_1960_2014.csv'
-crime=pd.read_csv(path4)
-print(crime.head())
+iris = pd.read_csv(path10,names = ['sepal_length','sepal_width', 'petal_length', 'petal_width', 'class'])
+print(iris.head())
+print(pd.isnull(iris).sum())
+iris.iloc[10:20,2:3]=np.nan
+print(iris.head(20))
+print("xxxxxxxxxxxxx")
+# iris.petal_length.fillna(1,inplace=True)
+# print(iris.head(20))
+iris.fillna(1,inplace=True)
+print(iris.head(20))
 
+# del iris['class']
+xx=iris.drop('class',axis=1)
+print(xx.head())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+iris=iris.dropna(how='any')
+print(iris.head())
